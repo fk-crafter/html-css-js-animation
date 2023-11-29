29469056fbfb973278261406fdb1fb39b0432611
Fofo <127132293+fk-crafter@users.noreply.github.com>
2023-11-29 08:54:31 +0100

    Update main.js

--- menu-interaction-animation/main.js
+++ menu-interaction-animation/main.js
@@ -1,5 +1,11 @@
 const navigation = document.querySelector(".navigation");
 
+document.addEventListener("click", (event) => {
+    if (!navigation.contains(event.target)) {
+        navigation.classList.remove("active");
+    }
+});
+
 navigation.addEventListener("click", () => {
     navigation.classList.toggle("active");
-})
+});
